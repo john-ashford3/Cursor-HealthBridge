739b2012a12a9b7993b93a13b65284aa513d92f9
john-ashford3 <124085277+john-ashford3@users.noreply.github.com>
2024-11-19 17:08:55 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cd /home/site/wwwroot
+
+# Create and activate virtual environment
+python -m venv antenv
+source antenv/bin/activate
+
+# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
-gunicorn --bind=0.0.0.0:8000 wsgi:app
+
+# Start Gunicorn
+gunicorn --bind=0.0.0.0:8000 application:app
